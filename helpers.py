d529655a572dfb28a418e5d700b75ca577d112f1
minkxx <aryuokk@gmail.com>
2024-04-29 15:33:40 +0530

    change

--- helpers.py
+++ helpers.py
@@ -3,9 +3,10 @@ import pytube
 def progress_bar(stream, chunk, bytes_remaining):
     current = ((stream.filesize - bytes_remaining)/stream.filesize)
     percent = ('{0:.2f}').format(current*100)
-    progress = int(40*current)
-    status = '█' * progress + '-' * (40 - progress)
-    print(f"Downloading: {status} {percent} %")
+    bar_length = 40
+    progress = int(bar_length*current)
+    status = '█' * progress + '-' * (bar_length - progress)
+    print(f"Downloading: {status} {percent} %", flush=True)
 
 def complete_status(stream, file_path):
     print(f"DOWNLOADED : {stream.title}\nMB : {stream.filesize_mb}")
